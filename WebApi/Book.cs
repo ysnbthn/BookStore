b9cd2d9d46b5a132a5a88986ced9bbe3f4d33dab
ysnbthn <yasinbatuhanozyurek@gmail.com>
2022-01-26 17:58:11 +0300

    Finish Book and Book controller basic functions

--- WebApi/Book.cs
+++ WebApi/Book.cs
@@ -1,10 +1,16 @@
 
+using System;
+
 namespace WebApi.Controllers
 {
     public class Book
     {
+        // Personal Growth GenreID 1, 
+        // Science Fiction GenreID 2 
         public int ID { get; set; }
         public string Title { get; set; }
-        public int GenreID { get; set; }   
+        public int GenreID { get; set; }
+        public int PageCount { get; set; }
+        public DateTime PublishDate { get; set; }
     }
 }
--- WebApi/Controllers/BookController.cs
+++ WebApi/Controllers/BookController.cs
@@ -1,10 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 namespace WebApi.Controllers
 {
     [ApiController] // api controller olduğunu belirt
-    [Route("[controller]s")] // site/BookController/endpoint + s şeklinde ulaşılacak
+    [Route("[controller]s")] // site/Book + s şeklinde ulaşılacak
     public class BookController : ControllerBase
     {
-        
+        // Dummy Data
+        private static List<Book> BookList = new List<Book>(){
+            new Book{ ID =1, Title = "Lean Startup", GenreID=1, PageCount=200, PublishDate= new DateTime(2001,06,12) },
+            new Book{ ID =2, Title = "Herland", GenreID=2, PageCount=250, PublishDate= new DateTime(2010,05,23) },
+            new Book{ ID =3, Title = "Dune", GenreID=2, PageCount=540, PublishDate= new DateTime(2001,12,21) }
+        };
+
+        [HttpGet] // sadece bir tane parametresiz get olabilir
+        public List<Book> GetBooks(){
+            List<Book> booklist = BookList.OrderBy(x => x.ID).ToList();
+            return booklist;
+        }
+
+        [HttpGet("{id}")] // from route
+        public Book GetByID(int id)
+        {
+            Book book = BookList.Where(x => x.ID == id).SingleOrDefault();
+            return book;
+        }
+
+        // [HttpGet] // from query
+        // query yerine route tercih etmeye çalış
+        // public Book Get([FromQuery] string id)
+        // {
+
+        //     Book book = BookList.Where(x => x.ID == Convert.ToInt32(id)).SingleOrDefault();
+        //     return book;
+        // }
+
+        // Post
+        [HttpPost]
+        public IActionResult AddBook([FromBody] Book newBook){
+            // önce id var mı kontrol et sonra id boş gelceği için isme çevir
+            Book book = BookList.SingleOrDefault(x => x.ID == newBook.ID);
+            if(book is not null){
+                return BadRequest();
+            }
+            BookList.Add(newBook);
+            return Ok();
+        }
+
+        // Put
+        [HttpPut("{id}")]
+        public IActionResult UpdateBook(int id, [FromBody] Book updatedBook){
+            var book = BookList.SingleOrDefault(x=>x.ID == id);
+            if(book is null){
+                return BadRequest();
+            }
+            // Şuan EF Core olmadığı için elle tek tek kontrol et
+            book.GenreID = updatedBook.GenreID != default ? updatedBook.GenreID : book.GenreID;
+            book.PageCount = updatedBook.PageCount != default ? updatedBook.PageCount : book.PageCount;
+            book.PublishDate = updatedBook.PublishDate != default ? updatedBook.PublishDate : book.PublishDate;
+            book.Title = updatedBook.Title != default ? updatedBook.Title : book.Title;
+
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteBook(int id){
+            Book book = BookList.SingleOrDefault(x => x.ID == id);
+            if(book == null){
+                return BadRequest();
+            }
+            BookList.Remove(book);
+            return Ok();
+        }
+
+
     }
 }
